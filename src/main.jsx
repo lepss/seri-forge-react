f1fda9e039c6a25039fd4f714d1415ebde05d17f
lepss <a.miotton@gmail.com>
2025-05-28 11:53:03 +0200

    page change scroll to top auto

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,12 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
+import { ScrollToTop } from "./utils/scrollToTop.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
+      <ScrollToTop />
       <App />
     </BrowserRouter>
   </StrictMode>
